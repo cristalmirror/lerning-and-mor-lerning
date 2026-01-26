d01f90dc7ad18fb0a039c2195e46f6f38547c1f2
cristalmirror <crisrojari@gmail.com>
2026-01-26 12:56:11 -0300

    second\ commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@ using namespace std;
 extern "C" {
     void saludar(); // Replace with actual C library header
     int sumar(int a, int b); // Declaration of the C function(Rust function)
+    int loop_func();
 }
 
 int main() {
     saludar(); // Call the C function
     int res = sumar(5, 7); // Call the Rust function
-    cout << "Resultado de sumar desde Rust: " << res <<endl;
+    cout << "Resultado de sumar desde Rust: " << res <<endl <<endl;
+    loop_func();
     return 0;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,9 @@ pub extern "C" fn sumar(a: i32, b: i32) -> i32 {
     println!("Hello, world!, from Rust! Code, your numbers are: {} and {}", a, b);
     return a + b;
 }
+#[unsafe(no_mangle)]
+pub extern "C" fn loop_func() {
+    for i in 1..10 {
+        println!("Numero: [{}] Rust",i);
+    }
+}
