a3ad560fa7f1dd31df0ef4c2477d02cdc285d7ca
cristalmirror <crisrojari@gmail.com>
2026-01-27 00:08:45 -0300

    problems in the object indexations

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+struct Obj;
+
+// Declarations of external C and Rust functions
 extern "C" {
     void saludar(); // Replace with actual C library header
     void despedir();
     int sumar(int a, int b); // Declaration of the Rust function
     void loop_func();
+    Obj* create_object(int id);
+    int object_comand(Obj* obj, int command,int num);
+    void destroy_object(Obj* obj);
 }
 
-int main() {
+// Main function
+int main(int argc,char *argv[]) {
     saludar(); // Call the C function
     int res = sumar(5, 7); // Call the Rust function
     cout << "Resultado de sumar desde Rust: " << res <<endl <<endl;
+    
+    int comand = stoi(argv[1]);
+    int num = stoi(argv[2]);
+    Obj* obj = create_object(42); // Create an object with ID 42
+    int comand_res = object_comand(obj, comand, num);
+    cout << "Resultado de object_comand: " << comand_res << endl << endl;
+    destroy_object(obj); // Clean up the object
     loop_func();
     despedir(); // Call the C function
+
+
     return 0;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,83 @@
+pub struct Object{
+    id_sesion: i32,
+    state: bool,
+}
+
+impl Object {
+    
+    fn new(id: i32) ->Self {
+        Object {
+            id_sesion: id,
+            state: false,
+        }
+    }
+
+    //pribvate methods
+    fn activate(&mut self,n: i32) {
+        self.state = true;
+        println!("Object {} activated", self.id_sesion);
+        self.processing_data(n);
+    }
+
+    fn deactivate(&mut self) {
+        self.state = false;
+        println!("Object {} deactivated", self.id_sesion);
+    }
+    fn processing_data(&self, n: i32) -> i32 {
+        if self.state {
+            println!("Processing data: {} in object {}", n, self.id_sesion);
+            n * 2 // Example processing
+        } else {
+            println!("Object {} is not active. Cannot process data.", self.id_sesion);
+            0
+        }
+        
+    }
+
+    //dispacher methods
+    pub fn start_processing(&mut self,comand: i32, n: i32) -> i32 {
+        match comand {
+            1 => {
+                self.activate(n);
+                0
+            },
+            2 => {
+                self.deactivate();
+                0
+            },
+            _ => {
+                println!("Invalid command");
+                -1
+            }
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn create_object(id: i32) -> *mut Object {
+    Box::into_raw(Box::new(Object::new(id)))
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn object_comand(obj_ptr: *mut Object, cnmd: i32, n: i32) {
+    let obj = unsafe {
+        assert!(!obj_ptr.is_null());
+        &mut *obj_ptr
+    };
+    obj.start_processing(cnmd, n);
+} 
+
+#[unsafe(no_mangle)]
+pub extern "C" fn destroy_object(obj_ptr: *mut Object) {
+    if !obj_ptr.is_null() {
+        unsafe {
+            let _ = Box::from_raw(obj_ptr);
+        }
+    }
+}
+
+
+
 #[unsafe(no_mangle)]
 pub extern "C" fn sumar(a: i32, b: i32) -> i32 {
     
