41dea5bbd5179ee6700bea799884023e4407613e
cristalmirror <crisrojari@gmail.com>
2026-04-24 20:09:17 -0300

    actualizacion 0.0.2

--- src/funcion.c
+++ src/funcion.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-#include"../includes/list.h"
+#include"../includes/linker.h"
 
 void saludar() {
     printf("¡Hola desde la función C!\n");
