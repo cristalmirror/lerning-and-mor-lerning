3280f0b4678ec8c0b3637ad6cb9420168f6960a2
cristalmirror <crisrojari@gmail.com>
2026-04-23 23:58:29 -0300

    update 0.0.1

--- src/list.c
+++ src/list.c
@@ -6,5 +6,9 @@ void init_struct_GDT(struct vertex **v) {
 }
 
 void construction_GDT(struct vertex **v, char ***matrix,int file, int colums) {
+    struct vertex *aux;
     struct vertex *act = (struct vertex *)malloc(sizeof(struct vertex));
+
+    
+
 }
